ac3c2fa812e3e4703f67253813f882cecf72a69a
hp <abouluo@gmail.com>
2016-03-25 23:39:49 +0800

    添加dom4j

--- src/main/java/com/aoeai/generator/android/controller/IndexController.java
+++ src/main/java/com/aoeai/generator/android/controller/IndexController.java
@@ -1,9 +1,17 @@
 package com.aoeai.generator.android.controller;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
 
 @Controller
 public class IndexController {
@@ -22,7 +30,38 @@ public class IndexController {
     @RequestMapping("/createAndroidCode")
     public String createAndroidCode(String xmlCode, Model model) {
         model.addAttribute("name", xmlCode);
+
+        try {
+            try {
+                parseXml(xmlCode);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+
         return "index";
     }
 
+
+    private void parseXml(String xml) throws DocumentException, UnsupportedEncodingException {
+        SAXReader saxReader = new SAXReader();
+        Document doc = saxReader.read(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        Element root = doc.getRootElement();
+        System.out.println("根节点："+root.getName()+",内容："+root.attributeValue("id"));
+
+        getElement(root);
+    }
+
+    private static void getElement(Element element){
+        List list = element.elements();
+        //递归方法
+        for (Object aList : list) {
+            Element chileEle = (Element) aList;
+            System.out.println("节点：" + chileEle.getName() + ",内容：" + chileEle.attributeValue("id"));
+            getElement(chileEle);
+        }
+    }
 }
+
